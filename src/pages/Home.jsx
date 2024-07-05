c88292f5c804ab25b9f997efd36d6bf34dc6bbb5
Natalie Lukaschyk <N.Janke@live.de>
2024-07-05 22:20:42 +0200

    add styling, rootlayout, home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <>
+      <h2>Natalie Lukaschyk 🐨</h2>
+      <img src="public/Natalie.jpeg" alt="Image of Natalie" />
+      <p>Frontend Developer in Training</p>
+    </>
+  );
+}
---
+++ src/pages/RootLayout.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/Footer";
+import { Outlet } from "react-router-dom";
+
+export function RootLayout() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
