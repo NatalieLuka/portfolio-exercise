a254644a6d9db3b2fc17dc65011ced080b6e1832
Natalie Lukaschyk <N.Janke@live.de>
2024-07-09 16:18:01 +0200

    update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,14 @@ export function Contact() {
   return (
     <div>
       <h3>How to contact me</h3>
+      <p>name: Natalie Lukaschyk </p>
+      <p>adress: blablastreet 5, 53111 Bonn </p>
+      <p>email: N.janke@live </p>
+      <p>
+        <a href="https://www.instagram.com/kunterbunte_buchliebe/?hl=de">
+          Instagram
+        </a>
+      </p>
     </div>
   );
 }
